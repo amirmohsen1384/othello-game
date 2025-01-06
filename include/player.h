362a45b01126ab8554ecf69615d4fb20bc720e53
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-07 01:05:47 +0330

    Defined the player

---
+++ include/player.h
@@ -0,0 +1,13 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "strings.h"
+
+/* The player containing different information */
+typedef struct Player {
+    String _name;
+    int _count;
+}
+Player;
+
+#endif
