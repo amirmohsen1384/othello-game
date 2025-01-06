72d5b58d43d8325bb800089279db7f77667975de
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-07 01:09:33 +0330

    Defined the main entry of the program.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "include/player.h"
+
+int main() {
+    Player players[2] = {{{NULL, 0}, 0}, {{NULL, 0}, 0}};
+    bool _turn = false;
+    
+    return EXIT_SUCCESS;
+}
