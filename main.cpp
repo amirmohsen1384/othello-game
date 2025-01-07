e5f24749308bb509361b3c1bcfd4b664105e6317
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-07 08:50:48 +0330

    Removed the constants

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include "include/player.h"
 
-#define PLAYER 0
-#define OPPONENT 1
-
 int main() {
     Player players[2] = {{{NULL, 0}, 0}, {{NULL, 0}, 0}};
     bool _turn = PLAYER;
