e150f3cbfda9b100adb3c4580a76d91270949dec
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-07 08:46:43 +0330

    Added some constants

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "include/player.h"
 
+#define PLAYER 0
+#define OPPONENT 1
+
 int main() {
     Player players[2] = {{{NULL, 0}, 0}, {{NULL, 0}, 0}};
-    bool _turn = false;
+    bool _turn = PLAYER;
     
     return EXIT_SUCCESS;
 }
