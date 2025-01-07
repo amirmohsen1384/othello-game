f11bb6e461eb6eea4808a14fc036a1d977f90d04
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-07 12:51:59 +0330

    Improved the functionality and name of the string type.

---
+++ include/text.h
@@ -0,0 +1,35 @@
+#ifndef TEXT_H
+#define TEXT_H
+
+/* A basic type to work with dynamic text. */
+typedef struct Text {    
+    char*   _data;
+    int     _size;
+}
+Text;
+
+// Checks whether a string is empty or not.
+bool IsEmpty(const Text &);
+
+// Initialize a string with a constant array of bytes.
+void Initialize(Text &, const char *);
+
+// Get a line of text from the input stream.
+Text GetLine();
+
+// Free up the memory cosumed by the string.
+void Destroy(Text &);
+
+// Remove a character from the string.
+bool Remove(Text &, int);
+
+// Compare two different strings.
+int Compare(const Text & , const Text &, bool sensitive = true);
+
+// Construct a string based on a constant array of bytes.
+Text Create(const char *);
+
+// Prints out a string.
+void Print(const Text &);
+
+#endif
