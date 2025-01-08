ff62c378d7d58092b3a6483dbc5ea6b4e3f76155
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-08 11:44:32 +0330

    Moved some definitions to the player source file

--- src/game.cpp
+++ src/game.cpp
@@ -1,2 +1,118 @@
 #include "../include/game.h"
+#include <cstdlib>
 
+typedef enum Direction {
+    Top = 0,
+    Bottom,
+    Left,
+    Right,
+    TopLeft,
+    BottomRight,
+    TopRight,
+    BottomLeft
+} 
+Direction;
+
+bool IsLegal(const Table &table, PlayerNumber player, const Point &point, Direction direction) {
+    const int opponent = ~player;
+    Point temp = {-1, -1};
+    Cell *cell = nullptr;
+    Point pos = point;
+    int count = 0;
+    do {
+        switch(direction) {
+            case Top: {
+                temp = MoveTop(pos);
+                break;
+            }
+            case Bottom: {
+                temp = MoveBottom(pos);
+                break;
+            }
+            case Left: {
+                temp = MoveLeft(pos);
+                break;
+            }
+            case Right: {
+                temp = MoveRight(pos);
+                break;
+            }
+            case TopLeft: {
+                temp = MoveTopLeft(pos);
+                break;
+            }
+            case TopRight: {
+                temp = MoveTopRight(pos);
+                break;
+            }
+            case BottomLeft: {
+                temp = MoveBottomLeft(pos);
+                break;
+            }
+            case BottomRight: {
+                temp = MoveBottomRight(pos);
+                break;
+            }
+        }
+        if(!IsValid(table, temp) || IsEmpty(table, temp)) {
+            break;
+        }
+        pos = temp;
+        if(*(cell = PointAt(table, pos)) == opponent) {
+            ++count;
+        }
+    }
+    while(*cell != player);
+    if(cell != nullptr) {
+        if(*cell == player && count > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool IsLegal(const Table &table, PlayerNumber player, const Point &point) {
+    if(!IsEmpty(table, point)) {
+        return false;
+    }
+    else if(IsLegal(table, player, point, Top)) {
+        return true;
+    }
+    else if(IsLegal(table, player, point, Bottom)) {
+        return true;
+    }
+    else if(IsLegal(table, player, point, Left)) {
+        return true;
+    }
+    else if(IsLegal(table, player, point, Right)) {
+        return true;
+    }
+    else if(IsLegal(table, player, point, TopLeft)) {
+        return true;
+    }
+    else if(IsLegal(table, player, point, TopRight)) {
+        return true;
+    }
+    else if(IsLegal(table, player, point, BottomLeft)) {
+        return true;
+    }
+    else if(IsLegal(table, player, point, BottomRight)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+Coordinates GetLegalPoints(const Table &table, PlayerNumber player) {
+    Coordinates result = {NULL, 0};
+    for(int i = 0; i < table._width; ++i) {
+        for(int j = 0; j < table._height; ++j) {
+            Point point = {i, j};
+            if(IsLegal(table, player, point)) {
+                Append(result, {i, j});
+            }
+        }
+    }
+    return result;
+}
