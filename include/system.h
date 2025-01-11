b773a9a5ada0ca6c19f810fcb85a51799d1f9877
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-11 08:37:11 +0330

    Moved the logic of graphics to the system library

--- include/system.h
+++ include/system.h
@@ -1,7 +1,34 @@
-#ifndef MISC_H
-#define MISC_H
+#ifndef SYSTEM_H
+#define SYSTEM_H
 
 #include "point.h"
+#include "text.h"
+
+typedef enum {
+    Black = 0,
+    Gray,
+
+    Blue,
+    BrightBlue,
+
+    Green,
+    BrightGreen,
+
+    Red,
+    BrightRed,
+
+    Cyan,
+    BrightCyan,
+
+    Magenta,
+    BrightMagenta,
+
+    Yellow,
+    BrightYellow,
+
+    White
+}
+Color;
 
 // Clears the screen and resets the pointer to the beginning.
 void ClearConsole();
@@ -11,5 +38,17 @@ void Pause(int duration);
 
 // Waits for the user to press a key.
 int InputKey();
+
+// Sets the foreground color of the console.
+void SetForeground(const Color &color);
+
+// Prints a piece of text in a specific color.
+void PrintWith(const Text &text, const Color &color);
+
+// Prints a piece of text in a specific color.
+void PrintWith(const char *string, const Color &color);
+
+// Resets the foreground color of the console.
+void ResetForeground();
 
 #endif
--- src/system.cpp
+++ src/system.cpp
@@ -3,8 +3,7 @@
 #include <cstdlib>
 
 void ClearConsole() {
-    using namespace std;
-    std::system("cls");
+    system("cls");
 }
 
 int InputKey() {
@@ -34,4 +33,98 @@ int InputKey() {
 
 void Pause(int duration) {
     Sleep(duration * 1000);
+}
+
+WORD ToForeground(const Color &color) {
+    switch(color) {
+        case Black: {
+            return 0;
+        }
+        case Gray: {
+            return FOREGROUND_INTENSITY;
+        }
+        case Blue: {
+            return FOREGROUND_BLUE;
+        }
+        case BrightBlue: {
+            return FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+        }
+        case Green: {
+            return FOREGROUND_GREEN;
+        }
+        case BrightGreen: {
+            return FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+        }
+        case Red: {
+            return FOREGROUND_RED;
+        }
+        case BrightRed: {
+            return FOREGROUND_RED | FOREGROUND_INTENSITY;
+        }
+        case Cyan: {
+            return FOREGROUND_BLUE | FOREGROUND_GREEN;
+        }
+        case BrightCyan: {
+            return FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+        }
+        case Magenta: {
+            return FOREGROUND_BLUE | FOREGROUND_RED;
+        }
+        case BrightMagenta: {
+            return FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY;
+        }
+        case Yellow: {
+            return FOREGROUND_GREEN | FOREGROUND_RED;
+        }
+        case BrightYellow: {
+            return FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY;
+        }
+        case White: {
+            return FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
+        }
+        default: {
+            return 0;
+        }
+    }
+}
+
+WORD GetInitialInfo(HANDLE output) {
+    CONSOLE_SCREEN_BUFFER_INFO information;
+    GetConsoleScreenBufferInfo(output, &information);
+    return information.wAttributes;
+}
+
+void SetForeground(const Color &color) {
+    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // Converts the color into an integer
+    WORD value = ToForeground(color);
+
+    // Sets the foreground color
+    SetConsoleTextAttribute(output, value);
+}
+
+void PrintWith(const Text &text, const Color &color) {
+    // Create a reference to the output
+    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // Get the attributes of the output
+    WORD initial = GetInitialInfo(output);
+
+    // Print the text
+    SetForeground(color);
+    Print(text);
+
+    // Reset the attributes of the output
+    SetConsoleTextAttribute(output, initial);
+}
+
+void PrintWith(const char *string, const Color &color) {
+    Text text = Create(string);
+    PrintWith(text, color);
+    Destroy(text);
+}
+
+void ResetForeground() {
+    SetForeground(White);
 }
