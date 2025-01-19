6d2be551d28b5a56b19283aff6906f4b8db16168
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-19 20:11:45 +0330

    Defined the main point of the program

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
-#include "src/commands.h"
+#include "src/match.h"
+#include "src/system.h"
 
 int main() {
     using namespace std;
 
-    MatchInfo info;
-    InitializeMatch(info);
-    
-    StartGame(info);
+    // Defines a match and initializes it.
+    MatchInfo match;
+    InitializeMatch(match);
 
-    Delete(info);
+    // Launches a game and starts it.
+    LaunchMatch(match);
+    ClearConsole();
+
+    // Gives the match a play.
+    PlayMatch(match);
+    ClearConsole();
+
+    // Evaluates the reuslt.
+    EvaluateResult(match);
+    ClearConsole();
+
+    // Narrates the result.
+    NarrateResult(match);
+    ClearConsole();
 
     return EXIT_SUCCESS;
 }
