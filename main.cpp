ac579597173876c3affd09ef6631fc0427ce48c4
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-20 22:54:32 +0330

    Integrated the scoreboard with the match

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 // Deals with playing a match.
 void LoadReversi() {
     using namespace std;
+    ClearConsole();
 
     // Defines a match and initializes it.
     MatchInfo match;
@@ -26,19 +27,63 @@ void LoadReversi() {
     // Narrates the result.
     NarrateResult(match);
 
+    // Loads the scoreboard.
+    Scoreboard board;
+    Initialize(board);
+    LoadScoreboard(board);
+
+    // Calculates the area of the table.
+    const Dimension area = match._environment._width * match._environment._height;
+
+    // Finds the ranked player.
+    switch(match._status) {
+        case OpponentWon: {
+            RankedPlayer target;
+            Initialize(target, match._players[PLAYER_OPPONENT], area);
+            AddRankedPlayer(board, target);
+            break;
+        }
+        case UserWon: {
+            RankedPlayer target;
+            Initialize(target, match._players[PLAYER_USER], area);
+            AddRankedPlayer(board, target);
+            break;
+        }
+    }
+
+    // Saves the scoreboard
+    SaveScoreboard(board);
+
     // Removes the history after finishing the match.
     RemoveFile(GetSavegameFile());
     Delete(match);
 
     // Waits for the user to press a key.
-    cout << endl << "Press any key to exit..." << endl;
+    cout << endl << "Press any key to get back to the main menu..." << endl;
+    InputKey();
+}
+
+void ShowScoreboard() {
+    using namespace std;
+    ClearConsole();
+
+    // Loads the scoreboard.
+    Scoreboard board;
+    Initialize(board);
+    LoadScoreboard(board);
+
+    // Shows the scoreboard.
+    PrintScoreboard(board);
+
+    // Waits for the user to press a key.
+    cout << "Press any key to get back to the main menu..." << endl;
     InputKey();
 }
 
 int main() {
     using namespace std;
 
-    LoadReversi();
+    ShowScoreboard();
 
     return EXIT_SUCCESS;
 }
